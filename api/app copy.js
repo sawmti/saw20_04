64488cfba6b36620123b9a2ad1af5fd4ce39850a
Luis Romero <84204591+luisrun@users.noreply.github.com>
2021-07-06 23:21:18 -0400

    prueba_2_LR

--- api/app copy.js
+++ api/app copy.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require("body-parser");
 const path = require('path');
 
 const app = express()
@@ -11,9 +10,6 @@ app.use(function(req, res, next) { console.log(req.url); next(); });
 // Directly serve static content from /client
 app.use(express.static(root + '/client'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 // Simple REST API that returns some entities
 app.get('/api/entities', (req,res) => 
  res.send({ entities: 
@@ -22,11 +18,5 @@ app.get('/api/entities', (req,res) =>
     'Q15920'
    ]})
 );
-app.post('/api/anotaciones', (req,res) => 
- {
-   var entidad = req.body.entidadin
-   var anotacion = req.body.anotacionin
-  console.log("anotacion:"+anotacion+", entidad: "+entidad);
-res.end("anotacion: "+anotacion+", entidad: "+entidad);
- });
+
 module.exports = app
